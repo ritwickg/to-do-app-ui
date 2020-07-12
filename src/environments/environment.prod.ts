a9b1d73623a4368f261a34b7fa7cfd18e46d6229
@ritwickg2017 <ritwickg2017@gmail.com>
2020-07-12 11:23:39 +0530

    environment prod updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    aws_project_region: 'ap-south-1',
+    aws_cognito_identity_pool_id: 'ap-south-1:725755216375:userpool/ap-south-1_aLUZQ8lhn',
+    aws_cognito_region: 'ap-south-1',
+    aws_user_pools_id: 'ap-south-1_aLUZQ8lhn',
+    aws_user_pools_web_client_id: 'bi1peivapgipj7opiiv593mp1',
+    aws_appsync_region: 'ap-south-1',
+    aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+    apiDomain: 'https://i12topgc2m.execute-api.ap-south-1.amazonaws.com/dev',
 };
